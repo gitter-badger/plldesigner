6e664ca93c77e7248dd6648d3526d382c88693f6
Juan Felipe Osorio <jfosorio@gmail.com>
2014-08-18 22:12:10 +0200

    Test the loopcalculate functions

--- pll.py
+++ pll.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 '''
 First version of this class
-''' 
+'''
 
 from __future__ import (absolute_import, division, print_function)
 
 import numpy as np
 from numpy import sqrt
 import scipy.constants as k
-import plldesigner.pnoise as pn 
+from  .pnoise import Pnoise
 
 class AnalogPLL(object):
-    def __init__(self, order, Kvco, Navg=1, prescaler=1, plltype='integer', 
+    def __init__(self, order, Kvco, Navg=1, prescaler=1, plltype='integer',
                  filter_vals={}):
         self.order, self.Kvco, self.Navg = order, Kvco, Navg
         self.prescaler, self.plltype = prescaler, plltype
         self.filter_vals = filter_vals
-        
+
 
     def loopcalc(self, fc, pm, Lvco, Lvco_fr, DL, Temp=300.13):
         '''
@@ -29,11 +29,11 @@ class AnalogPLL(object):
           DL(dB)          Distortion of the Lvco by R1 allowed
           Temp(K)         K
           filter_val      Is a dictionary with all the filter components
-          
-                         
-          ToDo:           The distance from tp2 to tp1 should be calculated 
+
+
+          ToDo:           The distance from tp2 to tp1 should be calculated
                           if possible from the noise  '''
-                          
+
         self.fc, self.pm, self.Lvco = fc, pm, Lvco
         self.Lvco_fr, self.DL, self.Temp = Lvco_fr, DL, Temp
         Kvco = self.Kvco
@@ -66,14 +66,14 @@ class AnalogPLL(object):
             tp2 = tp/10
             C3 = C2/10 # C2 should not be that big
             R2 = tp2/C3
-            self.filter_vals = {'C1':C1, 'C2':C2, 'C3':C3, 'R1':R1, 'R2':R2, 
+            self.filter_vals = {'C1':C1, 'C2':C2, 'C3':C3, 'R1':R1, 'R2':R2,
                                 'Icp': Icp}
 
     def calcTF(self, fm):
         s = 2*k.pi*fm*1j
         Navg, Kvco, fvals = (self.Navg, self.Kvco, self.filter_vals)
         if self.order==2:
-            C1, C2, R1, Icp = (fvals['C1'], fvals['C2'], fvals['R1'], 
+            C1, C2, R1, Icp = (fvals['C1'], fvals['C2'], fvals['R1'],
                                fvals['Icp'])
             tp = R1*C1*C2/(C1+C2)
             tz = R1*C1
@@ -84,7 +84,7 @@ class AnalogPLL(object):
             Tfm = 1/(1+Gfm)
             Hfm =  Navg*Gfm/(1+Gfm)
         if self.order ==3:
-            C1, C2, C3, R1, R2, Icp = (fvals['C1'], fvals['C2'], fvals['C3'], 
+            C1, C2, C3, R1, R2, Icp = (fvals['C1'], fvals['C2'], fvals['C3'],
                                fvals['R1'], fvals['R2'],fvals['Icp'])
             tz = R1*C1
             Kvco = self.Kvco
@@ -98,14 +98,14 @@ class AnalogPLL(object):
             print('order not implemented, the noise can not be calculated')
             raise
         return(Hfm,Gfm,Tfm)
-    
+
     def calcfcpm(self,fm):
         Hfm, Gfm, Tfm = self.calcTF(fm)
-        
-        
-    
 
-    def filter_vn2(self, fm, Temp=300.13): 
+
+
+
+    def filter_vn2(self, fm, Temp=300.13):
         fvals = self.filter_vals
         s = 2*k.pi*fm*1j
         if self.order==2:
@@ -123,12 +123,12 @@ class AnalogPLL(object):
             vn2R2 = 4*k.k*Temp*R2*np.abs(HvnR2)**2
             vn2 = vn2R1+vn2R2
         return(vn2)
-    
+
     def pnoise_calc(Lin_ref, Lout_ref, fm, Mult=1, Div=1):
          phi2_in_ref = 2*10**(Lin_ref/10)
          Hfm = repmat(Hfm,size(phi2_in_ref,1),1)
          phi2_in_ref = phi2_in_ref*abs(Hfm)**2
-         Lin_ref = 10*log10(phi2_in_ref/2)+20*log10(Mult)+20*log10(1/Div) 
+         Lin_ref = 10*log10(phi2_in_ref/2)+20*log10(Mult)+20*log10(1/Div)
          # Filter the noise of  the output referred sources
          phi2_out_ref = 2*10**(Lout_ref/10)
          Tfm = repmat(Tfm,size(phi2_out_ref,1),1)
@@ -138,8 +138,9 @@ class AnalogPLL(object):
          phi2_tot = (sum(phi2_in_ref,1)+sum(phi2_out_ref,1))*Mult^2/Div^2
          # division
          Ltot = 10*log10(phi2_tot/2)
-         phi_int = pnoise_integrate(10*log10(phi2_tot/2),fm,fm(1),fm(end))
-         
+         pnLtot  = pn.Pnoise(fm, Ltot)
+         phi_int = pnLtot
+
     def __repr__(self):
         str_val  = 'Filter report \n'
         str_val += '============= \n'
@@ -147,32 +148,53 @@ class AnalogPLL(object):
         str_val += 'fc = {:2.3f} (MHz), pm = {:d} (deegres) \n'.format(self.fc/1e6,self.pm)
         str_val += 'Ideal values: \n'
         str_val += 'Icp = {:2.3f} (uA), R1 = {:2.3f} (Kohms), R2 = {:2.3f} (KoHms) \n'.format(
-            self.filter_vals['Icp']/1e-6, self.filter_vals['R1']/1e3, 
+            self.filter_vals['Icp']/1e-6, self.filter_vals['R1']/1e3,
             self.filter_vals['R2']/1e3)
         str_val += 'C1 = {:2.3f} (pf), C2 = {:2.3f} (pf), C3 = {:2.3f} (pf)\n'.format(
-        self.filter_vals['C1']/1e-12, self.filter_vals['C2']/1e-12, 
+        self.filter_vals['C1']/1e-12, self.filter_vals['C2']/1e-12,
         self.filter_vals['C3']/1e-12)
         return(str_val)
-        
-         
+
+
 class vco(object):
     def __init__(self, kvco, fo, vfo):
         self.kvco = kvco
         self.fo = fo
         self.vfo = vfo
         self.freq_vs_volt = lambda vcont : self.fo + (vcont - self.vfo) * self.kvco
-        
-        
+
+
 class AnalogPLLDict(AnalogPLL):
     def __init__(self, _dict):
         self.__dict__.update(_dict)
         self.loopcalc(_dict['fc'], _dict['pm'], _dict['Lvco'],
             _dict['Lvco_fr'], _dict['DL'], Temp=_dict['Temp'])
-       
-    
+
+
+""" Tests
+
+"""
+from numpy.testing import  assert_almost_equal
+
+def test_loopcalc():
+    myAnalogPLL=AnalogPLL(3,521.8e+06,Navg=55.22,prescaler=2,plltype='Integer')
+    myAnalogPLL.loopcalc(1e6,60.0,-107.8, 1e6, 0.7, 300)
+    # Assert filter values for a specific design
+    assert_almost_equal(myAnalogPLL.filter_vals['C1'], 4.2842e-10, 4)
+    assert_almost_equal(myAnalogPLL.filter_vals['C2'], 3.31384e-11, 4)
+    assert_almost_equal(myAnalogPLL.filter_vals['C3'], 3.31384e-12, 4)
+    assert_almost_equal(myAnalogPLL.filter_vals['Icp'], 516.691e-6, 4)
+    assert_almost_equal(myAnalogPLL.filter_vals['R1'], 1.3864303e3, 4)
+    assert_almost_equal(myAnalogPLL.filter_vals['R2'], 1.28688908e3, 4)
+
+def tests():
+    test_loopcalc()
+
+
 if __name__ == "__main__":
-    from numpy.testing import  assert_almost_equal
-    pll=AnalogPLL(3,5.218e+08,Navg=55.22,prescaler=2,plltype='fractionalN')
-    pll.loopcalc(1e6,60.0,-107.8, 1e6, 0.7, 300)
+    tests()
+
+
+
     
     
