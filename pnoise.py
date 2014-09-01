bdeeb6335439b6e64d4e399dff43984f6d48b19d
Juan Felipe Osorio <jfosorio@gmail.com>
2014-09-01 23:23:38 +0200

    interpolation return a object of the same type and works, the new object is sampled at the new frequency

--- pnoise.py
+++ pnoise.py
@@ -13,39 +13,13 @@ import scipy.interpolate as intp
 
 """ Help functions  """
 
-def __pnoise_point_slopes__(fi, ldbc_fi, slopes, fm):
-    """
-    Function to evaluate a asymptotic model of the phase noise
-    :param fi:
-    :param slopes:
-    :param ldbc:
-    :param fm:
-    :return: function
-    """
-    phi2 = 2*10**(ldbc_fi/10)
-    phi2 = phi2.reshape((1, len(phi2)))
-    phi2_matrix = np.repeat(phi2, len(fm), axis=0)
 
-    slopes = np.copy(slopes/10)
-    slopes = slopes.reshape((1, len(slopes)))
-    slopes_matrix = np.repeat(slopes, len(fm), axis=0)
-
-    fi = fi.reshape((1, len(fi)))
-    fi_matrix = np.repeat(fi, len(fm), axis=0)
-
-    fm = fm.reshape((len(fm),1))
-    fm_matrix = np.repeat(fm, fi.shape[1], axis=1)
-
-    phi2_fm = np.sum(phi2_matrix*(fm_matrix/fi_matrix)**slopes_matrix, axis=1)
-    ldbc_fm = 10*log10(phi2_fm/2)
-    return ldbc_fm
-
-def __pnoise_interp1d__(self, fi, ldbc_fi, fm):
-    '''Redifine the ordinate from the new fm to fi'''
-    func_intp = intp.interp1d(log10(fi), ldbc_fi, kind='linear')
-    ldbc = func_intp(log10(fm))
-    return ldbc
-
+# functions to handle the units
+__funits__ = {
+    "dBc/Hz": lambda x: x,
+    "rad/sqrt(Hz)": lambda x: 10 * log10(x ** 2 / 2),
+    "rad**/Hz": lambda x: 10 * log10(x / 2),
+    }
 
 
 class Pnoise(object):
@@ -84,18 +58,20 @@ There are different types of input functions:
         self.fi = None
         self.ldbc_fi = None
         self.slopes = None
+        self.func_ldbc=None
 
-        # functions to handle the units
-        __funits = {
-            "dBc/Hz": lambda x: x,
-            "rad/sqrt(Hz)": lambda x: 10 * log10(x ** 2 / 2),
-            "rad**/Hz": lambda x: 10 * log10(x / 2),
-        }
-        self.ldbc = __funits[units](np.array(pnfm))
+        self.ldbc = __funits__[units](np.array(pnfm))
+
+    @classmethod
+    def with_interpolation(cls, fm, pnfm, label=None, units='dBc/Hz'):
+        pnoise_class = cls(None, None, label=label, units='dBc/Hz')
+        pnoise_class.fm = fm
+        pnoise_class.ldbc = __funits__[units](np.array(pnfm))
         try:
-            self.func_ldbc = lambda fx : __pnoise_interp1d__(fm, self.ldbc, fx)
+            pnoise_class.func_ldbc = lambda fx : __pnoise_interp1d__(fm, pnoise_class.ldbc, fx)
         except:
-            self.func_ldbc = None
+            pnoise_class.func_ldbc = None
+        return pnoise_class
 
 
     @classmethod
@@ -118,9 +94,8 @@ There are different types of input functions:
         """
         pnoise_class = cls(None, None, label=label, units='dBc/Hz')
         pnoise_class.func_ldbc = func
-        cls.label = label
-        cls.fm = None
-        cls.ldbc = None
+        pnoise_class.label = label
+        return pnoise_class
 
     @classmethod
     def with_points_slopes(cls, fi, ldbc_fi, slopes, label=None):
@@ -146,7 +121,11 @@ There are different types of input functions:
         pnoise_class.slopes = slopes
         pnoise_class.ldbc_fi = ldbc_fi
         pnoise_class.func_ldbc = lambda fm: __pnoise_point_slopes__(fi, slopes, ldbc_fi, fm)
+        return pnoise_class
 
+    def interp1(self, fx):
+        pnoise_class = Pnoise(fx, self.func_ldbc(fx),label=self.label)
+        return pnoise_class
 
     def plot(self, *args, **kwargs):
         plt.ylabel('$\mathcal{L}$(dBc/Hz)')
@@ -221,6 +200,41 @@ There are different types of input functions:
         return self.phi_out
 
 
+def __pnoise_interp1d__(fi, ldbc_fi, fm):
+    '''Redifine the ordinate from the new fm to fi'''
+    func_intp = intp.interp1d(log10(fi), ldbc_fi, kind='linear')
+    ldbc = func_intp(log10(fm))
+    return ldbc
+
+
+def __pnoise_point_slopes__(fi, ldbc_fi, slopes, fm):
+    """
+    Function to evaluate a asymptotic model of the phase noise
+    :param fi:
+    :param slopes:
+    :param ldbc:
+    :param fm:
+    :return: function
+    """
+    phi2 = 2*10**(ldbc_fi/10)
+    phi2 = phi2.reshape((1, len(phi2)))
+    phi2_matrix = np.repeat(phi2, len(fm), axis=0)
+
+    slopes = np.copy(slopes/10)
+    slopes = slopes.reshape((1, len(slopes)))
+    slopes_matrix = np.repeat(slopes, len(fm), axis=0)
+
+    fi = fi.reshape((1, len(fi)))
+    fi_matrix = np.repeat(fi, len(fm), axis=0)
+
+    fm = fm.reshape((len(fm),1))
+    fm_matrix = np.repeat(fm, fi.shape[1], axis=1)
+
+    phi2_fm = np.sum(phi2_matrix*(fm_matrix/fi_matrix)**slopes_matrix, axis=1)
+    ldbc_fm = 10*log10(phi2_fm/2)
+    return ldbc_fm
+
+
 """
 Testing and other functions
 """
@@ -228,18 +242,39 @@ Testing and other functions
 from numpy.testing import assert_almost_equal
 
 
-def test_from_fm_and_ldbc():
+def test__init__(plot=False):
+    # Test __init__
     fm = np.logspace(3, 9, 100)
     lorentzian = Pnoise(fm, 10 * np.log10(1 / (fm * fm)), label='Lorentzian')
     white = Pnoise(fm, -120 * np.ones(fm.shape), label='white')
     added = white + lorentzian
+    added.label = "addition"
     assert_almost_equal(added.ldbc[0], -60, 4)
     assert_almost_equal(added.ldbc[-1], -120, 4)
     ix, = np.where(fm > 1e6)
     assert_almost_equal(added.ldbc[ix[0]], -117.2822, 4)
+    if plot:
+        lorentzian.plot()
+        white.plot()
+        added.plot()
+        plt.legend()
+        plt.show()
+
+def test_with_interpolation(plot=False):
+    fm = np.logspace(3, 9, 1000)
+    lorentzian = Pnoise.with_interpolation(fm, 10 * np.log10(1 / (fm * fm)), label='Lorentzian')
+    fi = np.logspace(3,9,4)
+    interpolated = lorentzian.interp1(fi)
+    ldbc_equation = 10 * np.log10(1 / (fi * fi))
+    assert_almost_equal(ldbc_equation, interpolated.ldbc, 4)
+    if plot:
+        lorentzian.plot()
+        interpolated.plot('o')
+        plt.show()
 
 
-def test_private_functions():
+
+def test_private_functions(plot=False):
     # test the new
     fi = np.array([1e4, 1e9])
     ldbc_fi = np.array([-40, -150])
@@ -254,5 +289,6 @@ def test_private_functions():
 
 
 if __name__ == "__main__":
-    test_from_fm_and_ldbc()
-    test_private_functions()
+    test__init__(plot=False)
+    test_private_functions(plot=False)
+    test_with_interpolation(plot=False)
