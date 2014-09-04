e1e3c0a42fc4a2105133ba0d41505c6ed0bce33f
Juan F. Osorio <j.osorio@nxp.com>
2014-09-04 10:58:14 +0200

    Fix PEP8

--- sdmod.py
+++ sdmod.py
@@ -18,6 +18,7 @@ class SDModulator(object):
     *arg  : Positional arguments for the different types of SDMOD
     **argk :
     """
+
     def __init__(self, modtype, *arg, **argk):
         """ """
         self.modtype = modtype
@@ -64,10 +65,10 @@ def gen_mash(order, N, K, init=()):
     # Assertions
     assert len(init) == order or len(init) == 0, (
         'Initial condition length must be equal to the order of the modulator')
-    assert order >= 1 and order <= 4, 'This orders have not been implemented'
+    assert 1 <= order <= 4, 'This orders have not been implemented'
 
     # Modulator of order 1
-    _maxvalue = 2**N-1
+    _maxvalue = 2 ** N - 1
     L = len(K)
     if order == 1:
         # initialize the registers
@@ -77,14 +78,14 @@ def gen_mash(order, N, K, init=()):
         if len(init) == 1:
             stat0[0] = init[0]
         for j in arange(1, L):
-            stat0[j] = stat0[j-1] + K[j-1]
+            stat0[j] = stat0[j - 1] + K[j - 1]
             if stat0[j] > _maxvalue:
                 over0[j] = 1
                 stat0[j] -= _maxvalue + 1
         sd = over0
         cycles = np.where(stat0 == 0)[0]
         if len(cycles) > 1:
-            cycles = cycles[1]-cycles[0]
+            cycles = cycles[1] - cycles[0]
         else:
             cycles = -1
 
@@ -101,11 +102,11 @@ def gen_mash(order, N, K, init=()):
 
         # Implement the SDM
         for j in arange(1, L):
-            stat0[j] = stat0[j-1] + K[j-1]
+            stat0[j] = stat0[j - 1] + K[j - 1]
             if stat0[j] > _maxvalue:
                 over0[j] = 1
                 stat0[j] -= _maxvalue + 1
-            stat1[j] = stat1[j-1] + stat0[j]
+            stat1[j] = stat1[j - 1] + stat0[j]
             if stat1[j] > _maxvalue:
                 over1[j] = 1
                 stat1[j] -= _maxvalue + 1
@@ -134,23 +135,23 @@ def gen_mash(order, N, K, init=()):
 
         # Implement the SDM
         for j in arange(1, L):
-            stat0[j] = stat0[j-1] + K[j-1]
+            stat0[j] = stat0[j - 1] + K[j - 1]
             if stat0[j] > _maxvalue:
                 over0[j] = 1
                 stat0[j] -= _maxvalue + 1
 
-            stat1[j] = stat1[j-1] + stat0[j]
+            stat1[j] = stat1[j - 1] + stat0[j]
             if stat1[j] > _maxvalue:
                 over1[j] = 1
                 stat1[j] -= _maxvalue + 1
 
-            stat2[j] = stat2[j-1] + stat1[j]
+            stat2[j] = stat2[j - 1] + stat1[j]
             if stat2[j] > _maxvalue:
                 over2[j] = 1
                 stat2[j] -= _maxvalue + 1
         sd = over0
         sd += over1 - np.hstack(([0], over1[:-1]))
-        sd += over2 - 2*np.hstack(([0], over2[:-1]))
+        sd += over2 - 2 * np.hstack(([0], over2[:-1]))
         sd += np.hstack(([0], [0], over2[:-2]))
 
         stat = stat0 + stat1 + stat2
@@ -178,32 +179,32 @@ def gen_mash(order, N, K, init=()):
         over3 = zeros(L, dtype=np.int)
         # Implement the SDM
         for j in arange(1, L):
-            stat0[j] = stat0[j-1] + K[j-1]
+            stat0[j] = stat0[j - 1] + K[j - 1]
             if stat0[j] > _maxvalue:
                 over0[j] = 1
                 stat0[j] -= _maxvalue + 1
 
-            stat1[j] = stat1[j-1] + stat0[j]
+            stat1[j] = stat1[j - 1] + stat0[j]
             if stat1[j] > _maxvalue:
                 over1[j] = 1
                 stat1[j] -= _maxvalue + 1
 
-            stat2[j] = stat2[j-1] + stat1[j]
+            stat2[j] = stat2[j - 1] + stat1[j]
             if stat2[j] > _maxvalue:
                 over2[j] = 1
                 stat2[j] -= _maxvalue + 1
-            stat3[j] = stat3[j-1] + stat2[j]
+            stat3[j] = stat3[j - 1] + stat2[j]
             if stat3[j] > _maxvalue:
                 over3[j] = 1
                 stat3[j] -= _maxvalue + 1
 
         sd = (over0 +
-               over1 - np.hstack(([0], over1[:-1])) +
-               over2 - 2*np.hstack(([0], over2[:-1])) +
-               np.hstack(([0], [0], over2[:-2])) +
-               over3 - 3*np.hstack(([0], over3[:-1])) +
-               3*np.hstack(([0], [0], over3[:-2])) -
-               np.hstack(([0], [0], [0], over3[:-3]))
+              over1 - np.hstack(([0], over1[:-1])) +
+              over2 - 2 * np.hstack(([0], over2[:-1])) +
+              np.hstack(([0], [0], over2[:-2])) +
+              over3 - 3 * np.hstack(([0], over3[:-1])) +
+              3 * np.hstack(([0], [0], over3[:-2])) -
+              np.hstack(([0], [0], [0], over3[:-3]))
         )
 
         stat = stat0 + stat1 + stat2 + stat3
@@ -214,6 +215,7 @@ def gen_mash(order, N, K, init=()):
             cycles = -1
 
     return sd, cycles
+
 
 def L_mash_dB(m, fm, fref, N=1.0):
     """ Phase noise theoretical value of noise produced by a mash111 SDM
@@ -236,34 +238,35 @@ def L_mash_dB(m, fm, fref, N=1.0):
     ------
     ldbc :
     """
-    return 10*log10((2*pi)**2/(12*fref)*(2*sin(pi*fm/fref))**(2*(m-1))/N**2)
+    return 10 * log10((2 * pi) ** 2 / (12 * fref) * (2 * sin(pi * fm / fref)) ** (2 * (m - 1)) / N ** 2)
+
 
 if __name__ == "__main__":
     from numpy.testing import assert_almost_equal
     import numpy.random as rnd
     # Test order one assert the mean value
-    floatnum = rnd.rand()*np.ones(100000)
+    floatnum = rnd.rand() * np.ones(100000)
 
     # order one
-    sequence, cycles = gen_mash(1, 19, (floatnum*2**19).astype(int))
+    sequence, cycles = gen_mash(1, 19, (floatnum * 2 ** 19).astype(int))
     assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
     # order two
-    sequence, cycles = gen_mash(2, 19, (floatnum*2**19).astype(int))
+    sequence, cycles = gen_mash(2, 19, (floatnum * 2 ** 19).astype(int))
     assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
 
     # order three
-    sequence, cycles = gen_mash(3, 19, (floatnum*2**19).astype(int))
+    sequence, cycles = gen_mash(3, 19, (floatnum * 2 ** 19).astype(int))
     assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
 
     # order three
-    sequence, cycles = gen_mash(3, 19, 0.25*np.ones(100000)*2**19)
+    sequence, cycles = gen_mash(3, 19, 0.25 * np.ones(100000) * 2 ** 19)
     assert_almost_equal(sequence.mean(), 0.25, 4)
 
     # order three
-    sequence, cycles = gen_mash(4, 19, 0.25*np.ones(100000)*2**19)
+    sequence, cycles = gen_mash(4, 19, 0.25 * np.ones(100000) * 2 ** 19)
     assert_almost_equal(sequence.mean(), 0.25, 4)
 
     # Using the class the test is done as:
     sd_mash = SDModulator('mash', 3, 19,
-                    (np.array([0.323232]*100000)*2**19).astype(int))
+                          (np.array([0.323232] * 100000) * 2 ** 19).astype(int))
     assert_almost_equal(sd_mash.seq.mean(), 0.323232, 4)
