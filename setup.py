2552e54b6277c6387059f2d0e647615a23269660
Juan F. Osorio <j.osorio@nxp.com>
2014-09-19 17:26:19 +0200

    sintaxis error

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='plldesigner',
       description='Python toolkit for PLLs design and phase noise analyisis',
       author='Juan F. Osorio',
       author_email='jfosorio@gmail.com',
-      url='https://github.com/jfosorio/plldesigner')
+      url='https://github.com/jfosorio/plldesigner'
      )
