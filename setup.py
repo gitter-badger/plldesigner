3b5e77419092db95bfdf1bb9978765c8108a2ed8
Juan F. Osorio <j.osorio@nxp.com>
2014-09-19 19:05:19 +0200

    fix some inconcistencies with the setup

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 
 setup(name='plldesigner',
       version='0.1',
-      py_modules=['plldesigner'],
+      py_modules=['plldesigner', 'plldesigner.sdmod','plldesigner.pll',
+                  'plldesigner.pnoise'],
       description='Python toolkit for PLLs design and phase noise analyisis',
       author='Juan F. Osorio',
       author_email='jfosorio@gmail.com',
