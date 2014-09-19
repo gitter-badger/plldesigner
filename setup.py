103a271a3a6b247363d618cb0c48ad91fc9b38b9
Juan F. Osorio <j.osorio@nxp.com>
2014-09-19 17:23:53 +0200

    the gitingnore is updated to recreate the change of directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='plldesigner',
       version='0.1',
-      py_modules=['pnoise', 'pll', 'sdmod'],
+      py_modules=['plldesigner'],
       description='Python toolkit for PLLs design and phase noise analyisis',
       author='Juan F. Osorio',
       author_email='jfosorio@gmail.com',
