d4c811917fbf31b4e3a4ed1d5b99587be00de2e3
Juan F. Osorio <j.osorio@nxp.com>
2014-09-19 16:33:40 +0200

    new setup file to install the library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='plldesigner',
+      version='0.1',
+      py_modules=['pnoise', 'pll', 'sdmod'],
+      description='Python toolkit for PLLs design and phase noise analyisis',
+      author='Juan F. Osorio',
+      author_email='jfosorio@gmail.com',
+      url='https://github.com/jfosorio/plldesigner')
+     )
